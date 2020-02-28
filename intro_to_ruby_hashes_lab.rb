869a9230e32a71abf5cf243a740ac3f5c3d2abc1
Steven Kandow <kando1sr@gmail.com>
2020-02-28 16:18:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,10 +25,10 @@ def monopoly_with_third_tier
 	      four_pieces_owned: 200
 	    },
 	    names: {
-	      reading: 1,
-	      pennsylvania: 2,
-	      b_and_o: 3,
-	      short: 4
+	      reading_railroad: {}
+	      pennsylvania_railroad: {}
+	      b_and_o_railroad: {}
+	      short_line: {}
 	    }
 	  }
 	}
